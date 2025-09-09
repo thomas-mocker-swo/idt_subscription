af011af021459c66aabe24965b4de2e4f560032a
Thomas.Mocker <thomas.mocker@softwareone.com>
2025-09-09 14:07:02 +0200

    demo pd

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "identity" {
-  name     = "${var.location_short}-identity-rg"
+  name     = "${var.location_short}-identity2-rg"
   location = var.location
   tags     = var.tags
 }
